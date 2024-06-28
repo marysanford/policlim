66db03bf021b3f51812043cbe1113a5b353c9a0f
giorgio-musto <giorgio.musto@gmail.com>
2024-06-28 16:40:40 +0200

    Add files via upload

---
+++ code/variable_creation/predictions_figures_update.R
@@ -0,0 +1,446 @@
+
+#### Setup ####
+
+rm(list=ls())
+setwd("C:/Users/giorg/OneDrive/Documenti/RA Silvia Pianta/Political Parties Climate Discourse")
+
+library(rio)
+library(lubridate)
+library(stargazer)
+library(ggplot2)
+library(labelled)
+library(countrycode)
+library(viridis)
+library(tidyverse)
+library(tmap)
+library(sp)
+library(data.table)
+library(readr)
+library(rgdal)
+library(RColorBrewer)
+library(terra)
+library(sf)
+library(cartogram)
+library(hrbrthemes)
+library(scales)
+library(zoo)
+
+
+
+#### Data cleaning ####
+
+# Import manifesto data
+import("Dati/manifesto_level_meta_24Jun24.csv") %>%
+  mutate(iso2c = countrycode(country, origin = 'country.name', destination = 'iso2c')) %>%
+  mutate(parfam_name = case_when(parfam_fixed == 10 ~ 'ECO',
+                                 parfam_fixed==20 ~ 'LEF',
+                                 parfam_fixed==30 ~ 'SOC',
+                                 parfam_fixed==40 ~ 'LIB',
+                                 parfam_fixed==50 ~ 'CHR',
+                                 parfam_fixed==60 ~ 'CON',
+                                 parfam_fixed==70 ~ 'NAT',
+                                 parfam_fixed==80 ~ 'AGR',
+                                 parfam_fixed==90 ~ 'ETH',
+                                 parfam_fixed==95 ~ 'SIP',
+                                 parfam_fixed==98 ~ 'DIV' )) -> predictions
+
+# Average climate salience by year and country
+predictions %>%
+  dplyr::select(-date, -country)  %>%
+  group_by(iso2c, year) %>%
+  summarise(climate_broad_mean = mean(final_broad, na.rm=T),
+            climate_broad_w_mean = sum(final_broad*(pervote))/
+              (sum(pervote))) -> predictions_cy
+
+
+
+#### Mapping old and new broad climate salience ####
+
+# World countries
+world <- st_as_sf(rnaturalearth::countries110)
+# Europe
+world %>%
+  filter(REGION_UN == "Europe" & NAME != "Russia") %>%
+  # Add unique ID column
+  mutate(id = row_number(), .before = NAME_IT) -> europe
+
+# Bounding box for continental Europe.
+europe.bbox <- st_as_sf(SpatialPolygons(list(Polygons(list(Polygon(
+  matrix(c(-25,29,45,29,45,70,-25,70,-25,29),byrow = T,ncol = 2)
+)), ID = 1)), proj4string = CRS(proj4string(rnaturalearth::countries110))))
+
+# Get polygons that are only in continental Europe.
+europe.clipped <- st_intersection(europe, europe.bbox)
+
+# Check
+europe.clipped %>% ggplot() + geom_sf()
+
+# Retain only relevant variables and clean
+europe.clipped %>%
+  dplyr::select(GEOUNIT, NAME_LONG, ISO_A2) %>%
+  rename(iso2c = ISO_A2, name_long = NAME_LONG,
+         geounit = GEOUNIT) %>%
+  # Fix France and Norway
+  mutate(iso2c = case_when(geounit == "France" ~  "FR",
+                           geounit == "Norway" ~  "NO", .default = iso2c)) -> europe.tidy
+
+
+### Map climate salience in the oldest and most recent election by country
+
+# Prepare
+predictions_cy %>%
+  group_by(iso2c) %>%
+  filter(year == min(year) | year == max(year)) %>%
+  mutate(election = case_when(year == min(year) ~ 1,
+                              year == max(year) ~ 2),
+         election_label = case_when(election == 1 
+                                    ~ "Oldest elections (1990-2006)",
+                                    election == 2 
+                                    ~ "Most recent elections (2013-2021)"),
+         election_label = reorder(election_label, election)
+         ) -> for_plot
+dplyr::full_join(europe.tidy, for_plot, by = "iso2c") %>%
+  filter(!is.na(election)) -> for_plot
+
+# Check time range of oldest elections and most recent elections
+for_plot %>%
+  group_by(election) %>%
+  summarise(min_year = min(year), max_year = max(year)) -> check
+# View(check)
+
+# Map (Mean climate salience)
+for_plot %>%
+  ggplot() +
+  geom_sf(data = europe.tidy) +
+  geom_sf(aes(fill = climate_broad_mean)) +
+  facet_wrap(~election_label) + theme_void() +
+  scale_fill_viridis_c(name = "Climate salience in manifestos",
+                       direction = -1,
+                       limits = c(0, .1),
+                       oob = squish,
+                       guide = guide_colorbar(barwidth = unit(50, units = "mm"),
+                                              barheight = unit(4, units = "mm"),
+                                              title.position = 'top',
+                                              title.theme = element_text(size=10),
+                                              title.hjust = 0.4,
+                                              nrow = 1, ticks = FALSE)) +
+  theme(legend.position = "bottom")
+ggsave("Figures/clim_salience_map_time.png",
+       width = 6.41, height = 3.67
+       )
+
+# Map (Climate salience weighed by vote share)
+for_plot %>%
+  ggplot() +
+  geom_sf(data = europe.tidy) +
+  geom_sf(aes(fill = climate_broad_w_mean)) +
+  facet_wrap(~election_label) + theme_void() +
+  scale_fill_viridis_c(name = "Climate salience in manifestos",
+                       direction = -1,
+                       limits = c(0, .1),
+                       oob = squish,
+                       guide = guide_colorbar(barwidth = unit(50, units = "mm"),
+                                              barheight = unit(4, units = "mm"),
+                                              title.position = 'top',
+                                              title.theme = element_text(size=10),
+                                              title.hjust = 0.4,
+                                              nrow = 1, ticks = FALSE)) +
+  theme(legend.position = "bottom")
+ggsave("Figures/clim_salience_w_map_time.png",
+       width = 6.41, height = 3.67
+)
+
+
+
+#### Plot time trends of salience by country, left-right ####
+
+
+### Left-Right based on party score
+
+# Clean
+summary(predictions$rile)
+predictions %>%
+  mutate(pol_orientation = case_when(rile < 0 ~  "Left-wing parties",
+                                     rile > 0 ~  "Right-wing parties")) %>%
+  filter(!is.na(pol_orientation)) %>%
+  group_by(iso2c, year, pol_orientation, country) %>%
+  summarise(climate_broad_mean = mean(final_broad, na.rm=T),
+            climate_broad_w_mean = sum(final_broad*(pervote))/
+              (sum(pervote))) -> for_plot
+
+# Plot
+for_plot %>%
+  ggplot() +
+  geom_line(aes(x = year, y = climate_broad_mean, color = pol_orientation),
+            lwd = 0.6) +
+  facet_wrap(~country, ncol = 7) +
+  scale_color_discrete(name = "Type of party",
+                       guide = guide_legend(title.position = "top",
+                                            title.hjust = 0.5)) +
+  scale_x_continuous(breaks = c(1992, 2005, 2017)) +
+  theme(legend.position = "bottom", axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        panel.border = element_rect(colour="black", fill = 'transparent'),
+        panel.background= element_rect(fill = 'transparent'),
+        legend.background=element_rect(fill = 'transparent'),
+        legend.key=element_rect(fill = 'transparent'),
+        strip.background=element_rect(fill = 'transparent'))
+ggsave("Figures/climate_rile.png",
+       width = 6.78, height = 5.02
+       )
+
+
+### Left-Right-Green parties
+
+# Clean
+predictions %>%
+  mutate(pol_orientation = case_when(rile < 0 & parfam_name != "ECO"
+                                     ~  "Left-wing parties",
+                                     rile > 0 & parfam_name != "ECO"
+                                     ~  "Right-wing parties",
+                                     parfam_name == "ECO" ~ "Green parties")) %>%
+  filter(!is.na(pol_orientation)) %>%
+  group_by(iso2c, year, pol_orientation, country) %>%
+  summarise(climate_broad_mean = mean(final_broad, na.rm=T),
+            climate_broad_w_mean = sum(final_broad*(pervote))/
+              (sum(pervote))) -> for_plot
+
+# Plot
+for_plot %>%
+  ggplot() +
+  geom_line(aes(x = year, y = climate_broad_mean, color = pol_orientation),
+            lwd = 0.6) +
+  facet_wrap(~country, ncol = 7) +
+  scale_color_manual(values = c("Green parties" = "#009900",
+                                "Left-wing parties" = "red",
+                                "Right-wing parties" = "blue"),
+                     name = "Type of party",
+                     guide = guide_legend(title.position = "top",
+                                            title.hjust = 0.5)) +
+  scale_x_continuous(breaks = c(1992, 2005, 2017)) +
+  theme(legend.position = "bottom", axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        panel.border = element_rect(colour="black", fill = 'transparent'),
+        panel.background= element_rect(fill = 'transparent'),
+        legend.background=element_rect(fill = 'transparent'),
+        legend.key=element_rect(fill = 'transparent'),
+        strip.background=element_rect(fill = 'transparent'))
+ggsave("Figures/climate_rile_green.png",
+       width = 6.78, height = 5.02
+)
+
+
+
+#### Plot time trends of salience by party family ####
+
+# Clean
+predictions %>%
+  # Sum salience by year and party family and count the n. of parties by
+  # party family
+  mutate(party_n = 1) %>%
+  group_by(year, parfam_name) %>%
+  summarise(salience = mean(final_broad, na.rm = T)) -> predictions_pfy
+
+# Fill holes with zero
+cbind(year = c(1990, 1990, 2021, 2021),
+      parfam_name = c("AGR", "SIP", "CON", "ETH"),
+      salience = c(0, 0, 0, 0)) %>% as.data.frame() %>%
+  mutate(year = as.numeric(year), salience = as.numeric(salience)) -> fill
+rbind(predictions_pfy, fill) -> predictions_pfy
+  
+# Compute rolling average
+predictions_pfy %>%
+  group_by(parfam_name) %>% arrange(year) %>%
+  mutate(salience_sm5 = zoo::rollmean(salience, k = 4, fill = salience)
+  ) -> predictions_pfy
+
+# # Smooth out the edges
+# predictions_pfy %>%
+#   group_by(parfam_name) %>% arrange(year) %>%
+#   mutate(salience_sm5 = case_when(year == 1990 ~ (salience+lead(salience)+lead(salience,n=2))/3,
+#                                   year == 1991 ~ (lag(salience)+salience+lead(salience)+lead(salience,n=2))/4,
+#                                   year == 2020 ~ (lag(salience,n=2)+lag(salience)+salience+lead(salience))/4,
+#                                   year == 2021 ~ (lag(salience,n=2)+lag(salience)+salience)/3,
+#                                   .default = salience_sm5)) -> predictions_pfy
+
+
+### Stacked plot with alphabetical ordering
+
+# Prepare
+predictions_pfy %>%
+  mutate(parfam_name = factor(parfam_name,
+                              levels = c("SOC", "SIP", "NAT", "LIB", "LEF", "ETH",
+                                         "ECO", "DIV", "CON", "CHR", "AGR"))) %>%
+  # Drop "DIV" party family (too few observations) 
+  filter(parfam_name != "DIV") -> for_plot
+
+# Plot
+for_plot %>%
+  ggplot() +
+  geom_area(aes(x = year, y = salience_sm5, fill = parfam_name)) +
+  scale_fill_viridis_d(name = "Party family", option = "H", direction = -1,
+                       guide = guide_legend(nrow = 2, title.position = "top",
+                                            title.hjust = 0.5)) +
+  theme(panel.background = element_rect(fill = "transparent"),
+        panel.grid = element_line(colour = "grey"),
+        legend.position = "bottom") +
+  xlab("Year") + ylab("Salience of Climate Change")
+ggsave("Figures/party_fam_stacked.png",
+       width = 6.05, height = 4.56
+       )
+
+
+### Stacked plot with right-left political orientation
+
+# Prepare
+predictions %>%
+  group_by(parfam_name) %>%
+  summarise(avg_rile = mean(rile, na.rm = T)) %>%
+  arrange(avg_rile) -> order
+predictions_pfy %>%
+  mutate(parfam_name = factor(parfam_name,
+                              levels = c("ECO", order$parfam_name[-2]))) %>%
+  # Drop "DIV" party family (too few observations)
+  filter(parfam_name != "DIV") -> for_plot
+
+# Plot
+for_plot %>%
+  ggplot() +
+  geom_area(aes(x = year, y = salience_sm5, fill = parfam_name)) +
+  scale_fill_manual(values = c("ECO" = "#00B050",
+                               "LEF" = "#7A0403FF",
+                               "SOC" = "#CB2A04FF",
+                               "ETH" = "#F66B19FF",
+                               "SIP" = "#FABA39FF",
+                               "AGR" = "#C7EF34FF",
+                               "LIB" = "#1AE4B6FF",
+                               "CHR" = "#36AAF9FF",
+                               "CON" = "#4662D7FF",
+                               "NAT" = "#30123BFF"),
+                    aesthetics = "fill",
+                    name = "Party family",
+                    guide = guide_legend(nrow = 2, title.position = "top",
+                                            title.hjust = 0.5)) +
+  theme(panel.background = element_rect(fill = "transparent"),
+        panel.grid = element_line(colour = "grey"),
+        legend.position = "bottom") +
+  xlab("Year") + ylab("Salience of Climate Change") 
+ggsave("Figures/party_fam_stacked_rile.png",
+       width = 6.05, height = 4.56
+       )
+
+
+### Line graph
+
+# Prepare
+predictions %>%
+  group_by(parfam_name) %>%
+  summarise(avg_rile = mean(rile, na.rm = T)) %>%
+  arrange(avg_rile) -> order
+predictions_pfy %>%
+  mutate(parfam_name = factor(parfam_name,
+                              levels = c("ECO", order$parfam_name[-2]))) -> for_plot
+  
+# Plot
+for_plot %>%
+  ggplot() +
+  geom_line(aes(x = year, y = salience_sm5, color = parfam_name)) +
+  scale_color_manual(values = c("ECO" = "#00B050",
+                                "LEF" = "#7A0403FF",
+                                "SOC" = "#CB2A04FF",
+                                "ETH" = "#F66B19FF",
+                                "SIP" = "#FABA39FF",
+                                "AGR" = "#C7EF34FF",
+                                "LIB" = "#1AE4B6FF",
+                                "CHR" = "#36AAF9FF",
+                                "CON" = "#4662D7FF",
+                                "NAT" = "#30123BFF"),
+                    aesthetics = "color",
+                    name = "Party family",
+                    guide = guide_legend(nrow = 2, title.position = "top",
+                                         title.hjust = 0.5)) +
+  theme(panel.background = element_rect(fill = "transparent"),
+        legend.position = "bottom", legend.key = element_blank()) +
+  xlab("Year") + ylab("Salience of Climate Change") 
+ggsave("Figures/party_fam_line.png",
+       width = 6.05, height = 4.56
+       )
+
+
+### Line graph wrapped by party family
+
+# Prepare
+predictions %>%
+  group_by(parfam_name) %>%
+  summarise(avg_rile = mean(rile, na.rm = T)) %>%
+  arrange(avg_rile) -> order
+predictions_pfy %>%
+  mutate(parfam_name = factor(parfam_name,
+                              levels = c("ECO", order$parfam_name[-2]))) -> for_plot
+
+# Plot V1
+for_plot %>%
+  ggplot() +
+  geom_line(aes(x = year, y = salience_sm5, color = parfam_name)) +
+  facet_wrap(~ parfam_name) +
+  scale_color_manual(values = c("ECO" = "#00B050",
+                                "LEF" = "#7A0403FF",
+                                "DIV" = "#A72615",
+                                "SOC" = "#CB2A04FF",
+                                "ETH" = "#F66B19FF",
+                                "SIP" = "#FABA39FF",
+                                "AGR" = "#C7EF34FF",
+                                "LIB" = "#1AE4B6FF",
+                                "CHR" = "#36AAF9FF",
+                                "CON" = "#4662D7FF",
+                                "NAT" = "#30123BFF"),
+                     aesthetics = "color",
+                     name = "Party family",
+                     guide = guide_legend(nrow = 2, title.position = "top",
+                                          title.hjust = 0.5)) +
+  scale_x_continuous(breaks = c(1991, 2000, 2010, 2019)) +
+  theme(legend.position = "bottom", axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        panel.border = element_rect(colour="black", fill = 'transparent'),
+        panel.background= element_rect(fill = 'transparent'),
+        legend.background=element_rect(fill = 'transparent'),
+        legend.key=element_rect(fill = 'transparent'),
+        strip.background=element_rect(fill = 'transparent')) +
+  xlab("Year") + ylab("Salience of Climate Change") 
+ggsave("Figures/party_fam_line_wrapped_v1.png",
+       width = 6.05, height = 4.56
+       )
+
+# Plot V2
+for_plot %>%
+  ggplot() +
+  geom_line(aes(x = year, y = salience_sm5, color = parfam_name)) +
+  facet_wrap(~ parfam_name, ncol = 3) +
+  scale_color_manual(values = c("ECO" = "#00B050",
+                                "LEF" = "#7A0403FF",
+                                "DIV" = "#A72615",
+                                "SOC" = "#CB2A04FF",
+                                "ETH" = "#F66B19FF",
+                                "SIP" = "#FABA39FF",
+                                "AGR" = "#C7EF34FF",
+                                "LIB" = "#1AE4B6FF",
+                                "CHR" = "#36AAF9FF",
+                                "CON" = "#4662D7FF",
+                                "NAT" = "#30123BFF"),
+                     aesthetics = "color",
+                     name = "Party family",
+                     guide = guide_legend(ncol = 1, title.position = "top",
+                                          title.hjust = 0.5)) +
+  scale_x_continuous(breaks = c(1991, 2000, 2010, 2019)) +
+  theme(legend.position = "right", axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        panel.border = element_rect(colour="black", fill = 'transparent'),
+        panel.background= element_rect(fill = 'transparent'),
+        legend.background=element_rect(fill = 'transparent'),
+        legend.key=element_rect(fill = 'transparent'),
+        strip.background=element_rect(fill = 'transparent')) +
+  xlab("Year") + ylab("Salience of Climate Change") 
+ggsave("Figures/party_fam_line_wrapped_v2.png",
+       width = 6.05, height = 4.56
+       )
+
